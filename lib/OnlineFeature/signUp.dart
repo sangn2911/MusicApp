6b5f6799ca6995fb9948c9e11e874c6ea8b1ee66
RAITEI\Nguyen Quang Sang <sang.nguyen2911@hcmut.edu.vn>
2020-05-12 17:28:27 +0700

    Musis Player OK for NOW

---
+++ lib/OnlineFeature/signUp.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:MusicApp/sizeConfig.dart';
+import 'package:MusicApp/Custom/color.dart';
+import 'package:MusicApp/Custom/customIcons.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:keyboard_avoider/keyboard_avoider.dart';
+
+class SignUp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment:CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: SizeConfig.screenHeight*52/640,),
+          logoWidget(),
+          SizedBox(height: SizeConfig.screenHeight*31/640,),
+          textInput(),
+          SizedBox(height: SizeConfig.screenHeight*35/640,),
+          signUpButton(),
+          SizedBox(height: SizeConfig.screenHeight*8/640,),
+          signIn(),
+
+        ],
+      ),
+    );
+  }
+
+  Widget logoWidget(){
+    return Container(
+      height: 60.0,
+      width: 250.0,
+      child: Icon(
+        IconCustom.mymusic,
+        color: ColorCustom.orange,
+        size: 100,
+      ),
+    );
+  }
+
+  Widget textInput(){
+    return Padding(
+      padding: EdgeInsets.only(left: SizeConfig.screenWidth*5/36,top: 0.0,right: SizeConfig.screenWidth*5/36,bottom: 0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            text("Email:"),
+            textField(hint: "example@example.com"),
+            SizedBox(height: SizeConfig.screenHeight*15/640,),
+            text("Username:"),
+            textField(hint: ""),
+            SizedBox(height: SizeConfig.screenHeight*15/640,),
+            text("Password:"),
+            textField(),
+            SizedBox(height: SizeConfig.screenHeight*15/640,),
+            text("Confirm password:"),
+            textField(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget text(String str){
+    return Text(
+      str,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 22,
+      ),
+    );
+  }
+
+  Widget textField({String hint = ""}){
+    return TextField(
+      style: TextStyle(
+        fontSize: 20.0,
+        fontFamily: 'Lato',
+        fontWeight: FontWeight.w400,
+        color: Colors.black,
+      ),
+      decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: TextStyle(
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.w300,
+            fontSize: 18.0,
+            color: Colors.black,
+          ),
+          contentPadding: EdgeInsets.only(left: 15, bottom: 11, top: 11, right: 15),
+          filled: true,
+          fillColor: ColorCustom.orange,
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+              borderSide: BorderSide(color: Colors.black)
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+              borderSide: BorderSide(color: Colors.black)
+          )
+      ),
+      showCursor: true,
+      cursorColor: Colors.black,
+    );
+  }
+
+  Widget signUpButton(){
+    return ButtonTheme(
+      height: 35,
+      minWidth: 165,
+      buttonColor: Colors.white,
+      child: RaisedButton(
+        onPressed: ((){
+          print("Sign Up");
+        }),
+        shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(15.0),
+            side: BorderSide(color: Colors.black)
+        ),
+        child: Text(
+          "Sign up",
+          style: TextStyle(
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.w400,
+            color: Colors.black,
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget signIn(){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          "Already signed up? ",
+          style: TextStyle(
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+            fontSize: 17,
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            print("Sign in");
+          },
+          child: Text(
+            "Sign in here!",
+            style: TextStyle(
+              fontFamily: 'Lato',
+              fontWeight: FontWeight.w400,
+              color: ColorCustom.orange,
+              fontSize: 17,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
